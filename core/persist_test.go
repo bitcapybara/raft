ea64c84036bc048ec3bffc3630db0ce2be83ad58
bitcapybara <1227686398@qq.com>
2021-02-22 22:35:11 +0800

    快照持久化器测试

--- core/persist_test.go
+++ core/persist_test.go
@@ -31,3 +31,31 @@ func TestDefaultRaftStatePersister_LoadRaftState(t *testing.T) {
 	t.Log(raftState)
 	t.Log(string(raftState.Entries[0].Data))
 }
+
+func TestDefaultSnapshotPersister_SaveSnapshot(t *testing.T) {
+
+	snapshot := Snapshot{
+		LastIndex: 1,
+		LastTerm: 1,
+		Data: []byte("testing测试数据"),
+	}
+
+	persister := NewSnapshotPersister("../data/snapshot.store")
+	err := persister.SaveSnapshot(snapshot)
+
+	if err != nil {
+		t.Errorf("保存 Snapshot 测试失败：%s\n", err.Error())
+	}
+}
+
+func TestDefaultSnapshotPersister_LoadSnapshot(t *testing.T) {
+
+	persister := NewSnapshotPersister("../data/snapshot.store")
+	snapshot, err := persister.LoadSnapshot()
+	if err != nil {
+		t.Errorf("读取 Snapshot 测试失败：%s\n", err)
+	}
+
+	t.Log(snapshot)
+	t.Log(string(snapshot.Data))
+}
